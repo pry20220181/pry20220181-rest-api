58af5abf7e1738d8cfde9fa8e0406809f10a5217
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-06-04 10:51:29 -0500

    add gitignore

---
+++ pry20220181-rest-api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace pry20220181_rest_api
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
