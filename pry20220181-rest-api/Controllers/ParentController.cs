8149d704a7e62b002c197dacea75d51ca2ab1ddc
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-07-29 19:25:39 -0500

    Finish get children by Parent, the id is getted from Token

--- pry20220181-rest-api/Controllers/ParentController.cs
+++ pry20220181-rest-api/Controllers/ParentController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using pry20220181_core_layer.Modules.Master.DTOs.Output;
 using pry20220181_core_layer.Modules.Master.Services;
+using pry20220181_rest_api.Utils;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace pry20220181_rest_api.Controllers
@@ -21,15 +22,16 @@ namespace pry20220181_rest_api.Controllers
         }
 
 
-        [HttpGet("{parentId}/children", Name = "GetChildren")]
+        [HttpGet("children", Name = "GetChildren")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [SwaggerResponse(200, "Get Children by Parent", typeof(List<ChildDTO>))]
-        public async Task<IResult> GetChildren([FromRoute] int parentId = 0)
+        public async Task<IResult> GetChildren()
         {
             try
             {
                 var user = HttpContext.User;
+                var parentId = Convert.ToInt32(user.Claims.FirstOrDefault(c => c.Type == CustomClaimTypes.EntityId).Value);
                 if (parentId == 0)
                 {
                     return Results.BadRequest();
