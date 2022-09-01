3bb2aff5068f442b7693d4e576e0234d6dc49d44
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-09-01 18:26:05 -0500

    Finish endpoint to register and auth parents

--- pry20220181-core-layer/Modules/Master/DTOs/Output/RegisteredParentDTO.cs
+++ pry20220181-core-layer/Modules/Master/DTOs/Output/RegisteredParentDTO.cs
@@ -8,8 +8,10 @@ namespace pry20220181_core_layer.Modules.Master.DTOs.Output
 {
     public class RegisteredParentDTO
     {
+
+        public string Username { get; set; }
         public int ParentId { get; set; }
-        public string UserId {get;set;}
-        public string Jwt {get;set;}
+        public string UserId { get; set; }
+        public string Jwt { get; set; }
     }
 }
